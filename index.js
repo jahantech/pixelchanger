4795d9d975e3834d249bd7e556f4c57d560d634d
JahanTech <jahantech@users.noreply.github.com>
2017-08-15 09:35:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var Jimp = require("jimp");
 
 Jimp.read("mypic.jpg").then(function (mypic) {
     
-    for (w=0;w<lenna.bitmap.width;w++){
+    for (w=0;w<mypic.bitmap.width;w++){
 
-		for (h=0;h<lenna.bitmap.height;h++){
+		for (h=0;h<mypic.bitmap.height;h++){
 			console.log(w+h)
-			if (lenna.getPixelColor(w,h) > 3535973887){
+			if (mypic.getPixelColor(w,h) > 3535973887){
 				console.log(lenna.getPixelColor(w,h))
-				lenna.setPixelColor(8913151,w,h)
+				mypic.setPixelColor(8913151,w,h)
 			}
 			else{
-				lenna.setPixelColor(0x0000000,w,h) 
+				mypic.setPixelColor(0x0000000,w,h) 
 			}
 		}
 	}
